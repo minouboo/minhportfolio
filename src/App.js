555c43bd980ffebd358e47bc5af23a4e80ad4303
minouboo <minouboo369@yahoo.com>
2023-09-09 18:46:09 +0700

    test navigation page home

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes> 
-        <Route path="/" element={<Home />} /> 
+        <Route path="/minhportfolio" element={<Home />} /> 
         <Route path="/IT" element={<ItExperiencePage />} /> 
       </Routes>
     </Router>
