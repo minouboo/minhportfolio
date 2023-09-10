7073030f7983c80aca7f65aa951534c9136ce22c
minouboo <minouboo369@yahoo.com>
2023-09-10 22:42:42 +0700

    IT Experience

---
+++ src/Pages/businessPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import IntroBusiness from '../components/BusinessExperience/introBusiness';
+
+
+
+const BusinessPage = () => {
+  
+  return (
+    <div className="App">
+      <IntroBusiness/>
+      
+    </div>
+
+    
+    
+  )
+}
+
+export default BusinessPage
